8f9f97f2ada4580705763bf60390c5e94a92b826
Myask-sl <208960411+Myask-sl@users.noreply.github.com>
2025-08-08 10:55:01 -0600

    more pierce work

--- src/main/java/invalid/myask/takes_an_illage/mixins/MixinEntityArrow_pierce.java
+++ src/main/java/invalid/myask/takes_an_illage/mixins/MixinEntityArrow_pierce.java
@@ -1,11 +1,14 @@
 package invalid.myask.takes_an_illage.mixins;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
 
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,6 +21,9 @@ import invalid.myask.takes_an_illage.api.IPierceArrow;
 
 @Mixin(EntityArrow.class)
 public abstract class MixinEntityArrow_pierce extends Entity implements IPierceArrow {
+
+    @Unique
+    public int takes_an_illage$initialPierces = 0;
 
     public MixinEntityArrow_pierce(World worldIn) {
         super(worldIn);
@@ -44,19 +50,36 @@ public abstract class MixinEntityArrow_pierce extends Entity implements IPierceA
         return takes_an_illage$piercesRemaining <= 0;
     }
 
+    @Inject(method = "onUpdate",
+    at = @At(value = "FIELD", target = "Lnet/minecraft/entity/projectile/EntityArrow;ticksInAir:I", opcode = Opcodes.PUTFIELD, ordinal = 2))
+    private void dontBounce(CallbackInfo ci, @Local MovingObjectPosition movingObjectPosition) {
+        if (movingObjectPosition.typeOfHit != MovingObjectPosition.MovingObjectType.ENTITY
+            || movingObjectPosition.entityHit.hurtResistantTime <= 0
+            || takes_an_illage$initialPierces == 0) return;
+        //undo bounce if this [was] piercing and is going through an entity that something [presumably this] hurt
+        this.motionX /= -0.10000000149011612D;
+        this.motionY /= -0.10000000149011612D;
+        this.motionZ /= -0.10000000149011612D;
+        this.prevRotationYaw -= 180;
+        this.rotationYaw -= 180;
+    }
+
     @Inject(method = "writeEntityToNBT", at = @At("TAIL"))
     private void writeMoreNBT(NBTTagCompound tagCompound, CallbackInfo ci) {
         if (takes_an_illage$piercesRemaining > 0) tagCompound.setInteger("pierces", takesAnIllage$getPierce());
+        if (takes_an_illage$initialPierces > 0) tagCompound.setInteger("init_pierces", takes_an_illage$initialPierces);
     }
 
     @Inject(method = "readEntityFromNBT", at = @At("TAIL"))
     private void readMoreNBT(NBTTagCompound tagCompound, CallbackInfo ci) {
         takesAnIllage$setPierce(tagCompound.getInteger("pierces"));
+        takes_an_illage$initialPierces = tagCompound.getInteger("init_pierces");
     }
 
     @Override
     public void takesAnIllage$setPierce(int number) {
         takes_an_illage$piercesRemaining = Math.max(number, 0);
+        takes_an_illage$initialPierces = takes_an_illage$piercesRemaining;
     }
 
     @Override
--- src/main/java/invalid/myask/takes_an_illage/mixins/MixinEntityArrow_xbowdamage.java
+++ src/main/java/invalid/myask/takes_an_illage/mixins/MixinEntityArrow_xbowdamage.java
@@ -2,14 +2,21 @@ package invalid.myask.takes_an_illage.mixins;
 
 import invalid.myask.takes_an_illage.api.IXbowArrow;
 import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.nbt.NBTTagCompound;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityArrow.class)
 public abstract class MixinEntityArrow_xbowdamage implements IXbowArrow {
+
+    @Unique
+    protected boolean takesAnIllage$isFixedDamage = false;
+
     @Shadow
     public abstract double getDamage();
 
@@ -19,8 +26,15 @@ public abstract class MixinEntityArrow_xbowdamage implements IXbowArrow {
         return takesAnIllage$isFixedDamage() ? (float) getDamage() : original;
     }
 
-    @Unique
-    protected boolean takesAnIllage$isFixedDamage = false;
+    @Inject(method = "writeEntityToNBT", at = @At("TAIL"))
+    private void writeMoreNBT(NBTTagCompound tagCompound, CallbackInfo ci) {
+        if (takesAnIllage$isFixedDamage()) tagCompound.setBoolean("fixed_damage", true);
+    }
+
+    @Inject(method = "readEntityFromNBT", at = @At("TAIL"))
+    private void readMoreNBT(NBTTagCompound tagCompound, CallbackInfo ci) {
+        takesAnIllage$setFixedDamage(tagCompound.getBoolean("pierces"));
+    }
 
     @Override
     public void takesAnIllage$setFixedDamage(boolean in) {
