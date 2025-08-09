931528d1b551f632ebc372724da9e3a109e4a97b
Myask-sl <208960411+Myask-sl@users.noreply.github.com>
2025-08-08 23:27:42 -0600

    try making particle-less extra explosions for weak computer

--- src/main/java/invalid/myask/takes_an_illage/IllageItems.java
+++ src/main/java/invalid/myask/takes_an_illage/IllageItems.java
@@ -7,6 +7,7 @@ import net.minecraft.block.material.MapColor;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
@@ -31,7 +32,8 @@ public class IllageItems {
                 ItemStack firework = new ItemStack(Items.fireworks);
                 NBTTagCompound nbt = new NBTTagCompound(),
                     nbtFireworks = new NBTTagCompound(),
-                    nbtLargeBall = new NBTTagCompound();
+                    nbtLargeBall = new NBTTagCompound(),
+                    nbtBang = new NBTTagCompound();
                 NBTTagList nbtExplosions = new NBTTagList();
                 int[] colors = new int[] {MapColor.blueColor.colorValue, MapColor.yellowColor.colorValue};
                 firework.setTagCompound(nbt);
@@ -41,6 +43,8 @@ public class IllageItems {
 
                 nbtLargeBall.setByte("Type", (byte)1);
                 nbtLargeBall.setIntArray("Colors", colors);
+                nbtBang.setByte("Type", (byte) -1); //none
+                nbtLargeBall.setIntArray("Colors", new int[]{ItemDye.field_150922_c[0]});
                 if (Config.creative_pregen_fireworks_extravagant) {
                     nbtLargeBall.setIntArray("FadeColors", colors);
                     nbtLargeBall.setBoolean("Trail", true);
@@ -53,18 +57,19 @@ public class IllageItems {
                     nbtLargeBall.setBoolean("has_twinkle", true);
                     nbtLargeBall.setBoolean("has_trail", true);
                 }
+                nbtExplosions.appendTag(nbtLargeBall);
                 for (int j = 1; j <= 7; j += 6 ) {
-                    nbtExplosions.appendTag(nbtLargeBall);
                     for (int i = 1; i <= 3; i++) {
                         nbtFireworks.setInteger("Flight", i);
                         tabbedItems.add(firework.copy());
                     }
                     if (j == 7) break;
-                    nbtExplosions.appendTag(nbtLargeBall);
-                    nbtExplosions.appendTag(nbtLargeBall);
-                    nbtExplosions.appendTag(nbtLargeBall);
-                    nbtExplosions.appendTag(nbtLargeBall);
-                    nbtExplosions.appendTag(nbtLargeBall);
+                    nbtExplosions.appendTag(nbtBang);
+                    nbtExplosions.appendTag(nbtBang);
+                    nbtExplosions.appendTag(nbtBang);
+                    nbtExplosions.appendTag(nbtBang);
+                    nbtExplosions.appendTag(nbtBang);
+                    nbtExplosions.appendTag(nbtBang);
                 }
                 super.displayAllReleventItems(tabbedItems);
             }
---
+++ src/main/java/invalid/myask/takes_an_illage/mixins/MixinEntityFireworkStarterFX_boom.java
@@ -0,0 +1,22 @@
+package invalid.myask.takes_an_illage.mixins;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.particle.EntityFX;
+import net.minecraft.client.particle.EntityFireworkStarterFX;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(EntityFireworkStarterFX.class)
+public abstract class MixinEntityFireworkStarterFX_boom extends EntityFX {
+    protected MixinEntityFireworkStarterFX_boom(World p_i1218_1_, double p_i1218_2_, double p_i1218_4_, double p_i1218_6_) {
+        super(p_i1218_1_, p_i1218_2_, p_i1218_4_, p_i1218_6_);
+    }
+
+    @WrapWithCondition(method = "onUpdate",
+    at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/EntityFireworkStarterFX;createBall(DI[I[IZZ)V", ordinal = 1))
+    private boolean wheresTheEarthShatteringKaboom (EntityFireworkStarterFX instance, double d5, int d6, int[] d7, int[] l, boolean k, boolean j, @Local byte boomType) {
+        return boomType == 0;
+    }
+}
