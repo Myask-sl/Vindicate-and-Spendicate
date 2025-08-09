c1070f6bacf05dee69b3eb735b8c9211f709444d
Myask-sl <208960411+Myask-sl@users.noreply.github.com>
2025-08-08 21:59:27 -0600

    add [vanilla] exploding fireworks to creative tab

--- src/main/java/invalid/myask/takes_an_illage/IllageItems.java
+++ src/main/java/invalid/myask/takes_an_illage/IllageItems.java
@@ -1,8 +1,15 @@
 package invalid.myask.takes_an_illage;
 
+import java.util.List;
+
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.material.MapColor;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 
 import invalid.myask.takes_an_illage.items.ItemXBow;
 
@@ -17,6 +24,35 @@ public class IllageItems {
             @Override
             public Item getTabIconItem() {
                 return XBOW;
+            }
+
+            @Override
+            public void displayAllReleventItems(List<ItemStack> tabbedItems) { //[sic] relevant
+                ItemStack firework = new ItemStack(Items.fireworks);
+                NBTTagCompound nbt = new NBTTagCompound(),
+                    nbtFireworks = new NBTTagCompound(),
+                    nbtLargeBall = new NBTTagCompound();
+                NBTTagList nbtExplosions = new NBTTagList();
+                int[] colors = new int[] {MapColor.blueColor.colorValue, MapColor.yellowColor.colorValue};
+                firework.setTagCompound(nbt);
+                nbt.setTag("Fireworks", nbtFireworks);
+                nbtFireworks.setTag("Explosions", nbtExplosions);
+                nbtLargeBall.setString("shape", "large_ball");
+                nbtLargeBall.setIntArray("colors", colors);
+                for (int j = 1; j <= 7; j += 6 ) {
+                    nbtExplosions.appendTag(nbtLargeBall);
+                    for (int i = 1; i <= 3; i++) {
+                        nbtFireworks.setInteger("Flight", i);
+                        tabbedItems.add(firework.copy());
+                    }
+                    if (j == 7) break;
+                    nbtExplosions.appendTag(nbtLargeBall);
+                    nbtExplosions.appendTag(nbtLargeBall);
+                    nbtExplosions.appendTag(nbtLargeBall);
+                    nbtExplosions.appendTag(nbtLargeBall);
+                    nbtExplosions.appendTag(nbtLargeBall);
+                }
+                super.displayAllReleventItems(tabbedItems);
             }
         };
 
